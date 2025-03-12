ec6f1d500f4d9590bdac9257ede3dac95878bd85
Lilya <Lilyakhelid@gmail.com>
2025-03-12 23:28:22 +0100

    ajout mcmc

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,10 @@ import pandas as pd
 import plotly.express as px
 import os
 import yaml
+import numpy as np
+import scipy.stats as stats
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
 
 #----Chemins des fichiers----
@@ -21,6 +25,29 @@ def load_cleaned_data(filepath=PROCESSED_DATA_PATH):
     """
     return pd.read_excel(filepath)
 
+def plot_fitted_distributions(data, distributions, variable_name="Données"):
+    """
+    Prend une liste de distributions aux données et affiche un graphique de comparaison.
+    """
+    x = np.linspace(min(data), max(data), 100)  
+
+    fig = ff.create_distplot([data], [variable_name], show_hist=True)
+
+
+    for name, dist in distributions.items():
+        params = dist.fit(data)
+        pdf_values = dist.pdf(x, *params)  # Calcul de la densité de probabilité
+        fig.add_trace(go.Scatter(x=x, y=pdf_values, mode='lines', name=name)) 
+
+    fig.update_layout(
+        title=f"Comparaison des distributions ajustées pour {variable_name}",
+        xaxis_title=variable_name,
+        yaxis_title="Densité",
+        legend_title="Distributions"
+    )
+
+    fig.show()
+
 def visualize_distribution(df):
     """
     Affiche l'histogramme interactif des hauteurs de crues.
@@ -32,16 +59,25 @@ def visualize_distribution(df):
     fig.update_layout(bargap=0.1)
     fig.show()
 
-def get_optimal_digue_height(df, percentile=0.95):
+def get_optimal_digue_height(df, dist, seuil_risque=0.01):
     """
-    Calcule la hauteur de digue protégeant contre un pourcentage donné des crues.
+    Calcule la hauteur optimale de la digue basée sur une distribution ajustée.
+
     Args:
         df (pd.DataFrame): Données nettoyées.
-        percentile (float): Seuil de protection , par defaut 0.95 pour 95%.
+        dist (rv_continuous): Distribution ajustée (ex: scipy.stats.norm, lognorm, gumbel_r).
+        seuil_risque (float): Probabilité maximale de dépassement acceptée (ex: 0.01 pour 1%).
+
     Returns:
-        float: Hauteur optimale de la digue.
+        float: Hauteur optimale de la digue en mètres.
     """
-    return df["Hauteur"].quantile(percentile) #revoir sa pertinence
+    # Ajustement de la distribution sur les données des hauteurs
+    params = dist.fit(df["Hauteur"])
+
+    # Calcul de la hauteur critique en fonction du seuil de risque
+    h_critique = dist.ppf(1 - seuil_risque, *params)
+
+    return h_critique
 
 def visualize_distribution_with_threshold(df, hauteur_opt):
     """
@@ -90,3 +126,4 @@ def load_hydraulic_params():
     with open(CONFIG_PATH, "r") as file:
         params = yaml.safe_load(file)
     return params["hydraulic_model"]
+
